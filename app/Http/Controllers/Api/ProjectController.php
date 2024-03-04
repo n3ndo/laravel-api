2b9f04fc85541296e1f5309f3087b22e15ab40a4
n3ndo <ferdinandof3lixbagnale@gmail.com>
2024-03-04 18:15:55 +0100

    MILESTONE 2 done

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -5,7 +5,17 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Project;
+
 class ProjectController extends Controller
 {
-    //
+    public function index()
+    {
+        $projects = Project::all();
+
+        return response()->json([
+            'success' => true,
+            'results' => $projects
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProjectController as ProjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/projects', [ProjectController::class, 'index']);
+
+Route::get('/test', function(){
+    return response()->json(['message' => 'ciao']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
